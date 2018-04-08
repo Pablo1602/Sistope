1973477e9936da16220f4ca316a71897890205f4
Unknown <ricardo.alvarezz@usach.cl>
2018-04-08 16:17:00 -0300

    lectura hecha
    creo que la lectura completa es ta funcionando, "creo..."

--- LAB 1/Main.c
+++ LAB 1/Main.c
@@ -1,4 +1,4 @@
-#include <string.h>
+ #include <string.h>
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <fcntl.h>
@@ -44,41 +44,60 @@ int main(int argc, char const *argv[]){
   //int cantidadImg, uBnarizar, uCasifica, mostrar;
   
   /*while ((c = getopt (argc, argv, "c:u:n:b:")) != -1){
-	    switch (c){
-	      	case 'c':
-	        	sscanf(optarg, "%d", &cantidadImg);
-	        	break;
-	      	case 'u':
-	        	sscanf(optarg, "%d", &uBnarizar);
-	        	break;
-	      	case 'n':
-		    	sscanf(optarg, "%d", &uCasifica);
-	        	break;
-	   	   	case 'b':
-		      	mostrar = 1;
-	        	break;
-	      	case '?':
-		        if (optopt == 'c' || optopt == 'u' || optopt == 'n' || optopt == 'b')
-		          fprintf (stderr, "Opcion -%c requiere un argumento.\n", optopt);
-		        else if (isprint (optopt))
-		          fprintf (stderr, "Opcion desconocida `-%c'.\n", optopt);
-		        else
-		          fprintf (stderr,"Opcion con caracter desconocido `\\x%x'.\n",optopt);
-	        	return 0;
-	      	default:
-	        	abort ();
-      	}
+		switch (c){
+			case 'c':
+				sscanf(optarg, "%d", &cantidadImg);
+				break;
+			case 'u':
+				sscanf(optarg, "%d", &uBnarizar);
+				break;
+			case 'n':
+				sscanf(optarg, "%d", &uCasifica);
+				break;
+			case 'b':
+				mostrar = 1;
+				break;
+			case '?':
+				if (optopt == 'c' || optopt == 'u' || optopt == 'n' || optopt == 'b')
+				  fprintf (stderr, "Opcion -%c requiere un argumento.\n", optopt);
+				else if (isprint (optopt))
+				  fprintf (stderr, "Opcion desconocida `-%c'.\n", optopt);
+				else
+				  fprintf (stderr,"Opcion con caracter desconocido `\\x%x'.\n",optopt);
+				return 0;
+			default:
+				abort ();
+		}
 	}
   exit(0);*/
 	
-	bmpFileHeader header;     /* cabecera */
-  unsigned char *imgdata;   /* datos de imagen */
+	
+	unsigned char *imgdata;   /* datos de imagen */
 	char* bm = (char*)malloc(sizeof(char)*2);
-  int imagen, numbytes;
+	int imagen, numbytes;
+	
+	unsigned int* size 		= (unsigned int*)malloc(sizeof(unsigned int));        /* Tamaño del archivo */
+  unsigned short* resv1 = (unsigned short*)malloc(sizeof(unsigned short));       /* Reservado */
+  unsigned short* resv2 	= (unsigned short*)malloc(sizeof(unsigned short));      /* Reservado */
+  unsigned int* offset 	= (unsigned int*)malloc(sizeof(unsigned int));
+
+
+  unsigned int* headersize	= (unsigned int*)malloc(sizeof(unsigned int));      /* Tamaño de la cabecera */
+  unsigned int* width = (unsigned int*)malloc(sizeof(unsigned int));               /* Ancho */
+  unsigned int* height = (unsigned int*)malloc(sizeof(unsigned int));          /* Alto */
+  unsigned short* planes= (unsigned short*)malloc(sizeof(unsigned short));                  /* Planos de color (Siempre 1) */
+  unsigned short* bpp = (unsigned short*)malloc(sizeof(unsigned short));             /* bits por pixel */
+  unsigned int* compress = (unsigned int*)malloc(sizeof(unsigned int));        /* compresión */
+  unsigned int* imgsize = (unsigned int*)malloc(sizeof(unsigned int));     /* tamaño de los datos de imagen */
+  unsigned int* bpmx = (unsigned int*)malloc(sizeof(unsigned int));                /* Resolución X en bits por metro */
+  unsigned int* bpmy = (unsigned int*)malloc(sizeof(unsigned int));                /* Resolución Y en bits por metro */
+  unsigned int* colors = (unsigned int*)malloc(sizeof(unsigned int));              /* colors used en la paleta */
+  unsigned int* imxtcolors = (unsigned int*)malloc(sizeof(unsigned int));      /* Colores importantes. 0 si son todos */
+  
 	
 	
 	
-  imagen = open("imagen_0.bmp", O_RDONLY);
+  imagen = open("imagen_1.bmp", O_RDONLY);
 	
 	
   
@@ -89,27 +108,71 @@ int main(int argc, char const *argv[]){
 	//numbytes = read(imagen, buffer, sizeof(char));
 	//printf("%s\n", buffer);
 	
-	numbytes = read(imagen, header, sizeof(bmpFileHeader));
-	printf("%d\n", header.size);
+	numbytes = read(imagen, size, sizeof(unsigned int));
+	printf("%d\n",size[0]);
+
+
+	numbytes = read(imagen, resv1, sizeof(unsigned short));
+	printf("%d\n",resv1[0] );
 	
-	/*
-	numbytes = read(imagen, buffer, sizeof(uint32_t));
-		printf("%s\n", buffer);
-	numbytes = read(imagen, buffer, sizeof(uint32_t));
-		printf("%s\n", buffer);
-	*/
+	numbytes = read(imagen, resv2, sizeof(unsigned short));
+	printf("%d\n",resv2[0] );
+
+	numbytes = read(imagen, offset, sizeof(unsigned int));
+	printf("%d\n",offset[0]);
+
+
+
+	numbytes = read(imagen, headersize, sizeof(unsigned int));
+	printf("%d\n",headersize[0]);
+
+	numbytes = read(imagen, width, sizeof(unsigned int));
+	printf("%d\n",width[0]);
+
+	numbytes = read(imagen, height, sizeof(unsigned int));
+	printf("%d\n",height[0]);
+
+	numbytes = read(imagen, planes, sizeof(unsigned short));
+	printf("%d\n",planes[0] );
+
+	numbytes = read(imagen, bpp, sizeof(unsigned short));
+	printf("%d\n",bpp[0] );
+
+	numbytes = read(imagen, compress, sizeof(unsigned int));
+	printf("%d\n",compress[0]);
+
+	numbytes = read(imagen, imgsize, sizeof(unsigned int));
+	printf("%d\n",imgsize[0]);
+
+	numbytes = read(imagen, bpmx, sizeof(unsigned int));
+	printf("%d\n",bpmx[0]);
+
+	numbytes = read(imagen, bpmy, sizeof(unsigned int));
+	printf("%d\n",bpmy[0]);
+
+	numbytes = read(imagen, colors, sizeof(unsigned int));
+	printf("%d\n",colors[0]);
+
+	numbytes = read(imagen, imxtcolors, sizeof(unsigned int));
+	printf("%d\n",imxtcolors[0]);
+
+	numbytes = read(imagen, imxtcolors, sizeof(unsigned int));
+	printf("%d\n",imxtcolors[0]);
+
+
+
+  close(imagen);
 	exit(0);
 	
   /* while ((numbytes = read(imagen, buffer, sizeof(char))) > 0){
-    printf("%s", buffer);
-    //write(fd2, &buffer, numbytes);
+	printf("%s", buffer);
+	//write(fd2, &buffer, numbytes);
 		
   }
    printf("\n");
 
  
   Cierre de archivos */
-  close(imagen);
  
 	return 0;
 }
