4df08f0dee7d8fda42878fda48d5a87b1363f86b
Unknown <pablo.cancino@usach.cl>
2018-04-08 19:06:18 -0300

    Arreglos y comprobación de lectura

--- LAB 1/Main.c
+++ LAB 1/Main.c
@@ -13,37 +13,34 @@
 
 //https://os.mbed.com/handbook/C-Data-Types    Conversion 
 
-
-
-
 //Funciones llamados a sistema http://sopa.dis.ulpgc.es/prog_c/FICHER.HTM
-
 
 typedef struct bmpFileHeader
 {
   /* 2 bytes de identificación */
-  unsigned int size;        /* Tamaño del archivo */
-  unsigned short resv1;       /* Reservado */
-  unsigned short resv2;       /* Reservado */
-  unsigned int offset;      /* Offset hasta hasta los datos de imagen */
+  unsigned int* size;        /* Tamaño del archivo */
+  unsigned short* resv1;       /* Reservado */
+  unsigned short* resv2;       /* Reservado */
+  unsigned int* offset;      /* Offset hasta hasta los datos de imagen */
 } bmpFileHeader;
 
 typedef struct bmpInfoHeader
 {
-  unsigned int headersize;      /* Tamaño de la cabecera */
-  unsigned int width;               /* Ancho */
-  unsigned int height;          /* Alto */
-  unsigned short planes;                  /* Planos de color (Siempre 1) */
-  unsigned short bpp;             /* bits por pixel */
-  unsigned int compress;        /* compresión */
-  unsigned int imgsize;     /* tamaño de los datos de imagen */
-  unsigned int bpmx;                /* Resolución X en bits por metro */
-  unsigned int bpmy;                /* Resolución Y en bits por metro */
-  unsigned int colors;              /* colors used en la paleta */
-  unsigned int imxtcolors;      /* Colores importantes. 0 si son todos */
+  unsigned int* headersize;      /* Tamaño de la cabecera */
+  unsigned int* width;               /* Ancho */
+  unsigned int* height;          /* Alto */
+  unsigned short* planes;                  /* Planos de color (Siempre 1) */
+  unsigned short* bpp;             /* bits por pixel */
+  unsigned int* compress;        /* compresión */
+  unsigned int* imgsize;     /* tamaño de los datos de imagen */
+  unsigned int* bpmx;                /* Resolución X en bits por metro */
+  unsigned int* bpmy;                /* Resolución Y en bits por metro */
+  unsigned int* colors;              /* colors used en la paleta */
+  unsigned int* imxtcolors;      /* Colores importantes. 0 si son todos */
 } bmpInfoHeader;
 
-
+void iniciador(bmpFileHeader* fh, bmpInfoHeader* ih);
+unsigned char* lectura(bmpFileHeader* fh, bmpInfoHeader* ih);
 
 int main(int argc, char const *argv[]){
 
@@ -77,116 +74,109 @@ int main(int argc, char const *argv[]){
 	}
   exit(0);*/
 	
-	
-	unsigned char *imgdata;   /* datos de imagen */
-	char* bm = (char*)malloc(sizeof(char)*2);
-	int imagen, numbytes;
-	
-	unsigned int* size 		= (unsigned int*)malloc(sizeof(unsigned int));        /* Tamaño del archivo */
-  unsigned short* resv1 = (unsigned short*)malloc(sizeof(unsigned short));       /* Reservado */
-  unsigned short* resv2 	= (unsigned short*)malloc(sizeof(unsigned short));      /* Reservado */
-  unsigned int* offset 	= (unsigned int*)malloc(sizeof(unsigned int));
-
-
-  unsigned int* headersize	= (unsigned int*)malloc(sizeof(unsigned int));      /* Tamaño de la cabecera */
-  unsigned int* width = (unsigned int*)malloc(sizeof(unsigned int));               /* Ancho */
-  unsigned int* height = (unsigned int*)malloc(sizeof(unsigned int));          /* Alto */
-  unsigned short* planes= (unsigned short*)malloc(sizeof(unsigned short));                  /* Planos de color (Siempre 1) */
-  unsigned short* bpp = (unsigned short*)malloc(sizeof(unsigned short));             /* bits por pixel */
-  unsigned int* compress = (unsigned int*)malloc(sizeof(unsigned int));        /* compresión */
-  unsigned int* imgsize = (unsigned int*)malloc(sizeof(unsigned int));     /* tamaño de los datos de imagen */
-  unsigned int* bpmx = (unsigned int*)malloc(sizeof(unsigned int));                /* Resolución X en bits por metro */
-  unsigned int* bpmy = (unsigned int*)malloc(sizeof(unsigned int));                /* Resolución Y en bits por metro */
-  unsigned int* colors = (unsigned int*)malloc(sizeof(unsigned int));              /* colors used en la paleta */
-  unsigned int* imxtcolors = (unsigned int*)malloc(sizeof(unsigned int));      /* Colores importantes. 0 si son todos */
-  
-
-  imagen = open("imagen_1.bmp", O_RDONLY);
-   /* Lectura BM */
-	numbytes = read(imagen, bm, sizeof(char)*2);
-	printf("%s\n", bm);
-	//Tamaño
-	numbytes = read(imagen, size, sizeof(unsigned int));
-	printf("%d\n",size[0]);
-	//reservado1
-	numbytes = read(imagen, resv1, sizeof(unsigned short));
-	printf("%d\n",resv1[0] );
-	//reservado2
-	numbytes = read(imagen, resv2, sizeof(unsigned short));
-	printf("%d\n",resv2[0] );
-	//inicio de los datos
-	numbytes = read(imagen, offset, sizeof(unsigned int));
-	printf("%d\n",offset[0]);
-	//Tamaño de la cabecera de bitmap
-	numbytes = read(imagen, headersize, sizeof(unsigned int));
-	printf("%d\n",headersize[0]);
-	//Ancho pixeles
-	numbytes = read(imagen, width, sizeof(unsigned int));
-	printf("%d\n",width[0]);
-	//Alto Pixeles
-	numbytes = read(imagen, height, sizeof(unsigned int));
-	printf("%d\n",height[0]);
-	//Numero de planos
-	numbytes = read(imagen, planes, sizeof(unsigned short));
-	printf("%d\n",planes[0] );
-	//Tamaño cada punto
-	numbytes = read(imagen, bpp, sizeof(unsigned short));
-	printf("%d\n",bpp[0] );
-	//Compreso?
-	numbytes = read(imagen, compress, sizeof(unsigned int));
-	printf("%d\n",compress[0]);
-	//tamaño imagen
-	numbytes = read(imagen, imgsize, sizeof(unsigned int));
-	printf("tamaño imagen %d\n",imgsize[0]);
-	//resolucion Horizontal
-	numbytes = read(imagen, bpmx, sizeof(unsigned int));
-	printf("%d\n",bpmx[0]);
-	//Resolución vertical
-	numbytes = read(imagen, bpmy, sizeof(unsigned int));
-	printf("%d\n",bpmy[0]);
-	//Tamaño de tabla colores
-	numbytes = read(imagen, colors, sizeof(unsigned int));
-	printf("%d\n",colors[0]);
-	//comtador de colores importantes
-	numbytes = read(imagen, imxtcolors, sizeof(unsigned int));
-	printf("%d\n",imxtcolors[0]);
-
-
-	imgdata = (unsigned char*)malloc(sizeof(char)*imgsize[0]);
-
-	numbytes = lseek(imagen,imgsize[0],SEEK_SET);
-
-	numbytes = read(imagen, imgdata, sizeof(unsigned char)*imgsize[0]);
-
-
-
-
-
-
-
-
-	//printf("aqui termina \n");
-	//int a = 3<1;
-	//printf("%d\n",a );
-
-
-  close(imagen);
+	unsigned char* imgdata;
+	bmpFileHeader* fh =(bmpFileHeader*)malloc(sizeof(bmpFileHeader));
+	bmpInfoHeader* ih =(bmpInfoHeader*)malloc(sizeof(bmpInfoHeader));
+	iniciador(fh, ih);
+	imgdata = lectura(fh, ih);
+	printf("%d\n", imgdata[0]);
 	exit(0);
-	
-  /* while ((numbytes = read(imagen, buffer, sizeof(char))) > 0){
-	printf("%s", buffer);
-	//write(fd2, &buffer, numbytes);
-		
-  }
-   printf("\n");
-
- 
-  Cierre de archivos */
  
 	return 0;
 }
 
+unsigned char* lectura(bmpFileHeader* fh, bmpInfoHeader* ih){
+	int imagen, numbytes;
+	char* bm = (char*)malloc(sizeof(char)*2);
 
+  	imagen = open("imagen_3.bmp", O_RDONLY);	
+  
+   /* Lectura BM */
+	numbytes = read(imagen, bm, sizeof(char)*2);
+	printf("%s\n", bm);
+	
+	//numbytes = read(imagen, buffer, sizeof(char));
+	//printf("%s\n", buffer);
+	
+	numbytes = read(imagen, fh->size, sizeof(unsigned int));
+	printf("Tamaño: %d\n",fh->size[0]);
+
+	numbytes = read(imagen, fh->resv1, sizeof(unsigned short));
+	printf("resv1: %d\n",fh->resv1[0] );
+	
+	numbytes = read(imagen, fh->resv2, sizeof(unsigned short));
+	printf("res2v: %d\n",fh->resv2[0] );
+
+	numbytes = read(imagen, fh->offset, sizeof(unsigned int));
+	printf("Inicio de los datos: %d\n",fh->offset[0]);
+
+
+	numbytes = read(imagen, ih->headersize, sizeof(unsigned int));
+	printf("Tamaño de la cabecera de bitmap: %d\n",ih->headersize[0]);
+
+	numbytes = read(imagen, ih->width, sizeof(unsigned int));
+	printf("Ancho pixeles: %d\n",ih->width[0]);
+
+	numbytes = read(imagen, ih->height, sizeof(unsigned int));
+	printf("Alto Pixeles: %d\n",ih->height[0]);
+
+	numbytes = read(imagen, ih->planes, sizeof(unsigned short));
+	printf("Numero de planos: %d\n",ih->planes[0]);
+
+	numbytes = read(imagen, ih->bpp, sizeof(unsigned short));
+	printf("Tamaño cada punto: %d\n",ih->bpp[0]);
+
+	numbytes = read(imagen, ih->compress, sizeof(unsigned int));
+	printf("Compreso?: %d\n",ih->compress[0]);
+
+	numbytes = read(imagen, ih->imgsize, sizeof(unsigned int));
+	printf("tamaño imagen: %d\n",ih->imgsize[0]);
+
+	numbytes = read(imagen, ih->bpmx, sizeof(unsigned int));
+	printf("resolucion Horizontal: %d\n",ih->bpmx[0]);
+
+	numbytes = read(imagen, ih->bpmy, sizeof(unsigned int));
+	printf("Resolución vertical: %d\n",ih->bpmy[0]);
+
+	numbytes = read(imagen, ih->colors, sizeof(unsigned int));
+	printf("Tamaño de tabla colores: %d\n",ih->colors[0]);
+
+	numbytes = read(imagen, ih->imxtcolors, sizeof(unsigned int));
+	printf("comtador de colores importantes: %d\n",ih->imxtcolors[0]);
+
+
+	unsigned char* imgdata = (unsigned char*)malloc(sizeof(unsigned char)*ih->imgsize[0]);   /* datos de imagen */
+	unsigned char* basura = (unsigned char*)malloc(sizeof(unsigned char));;
+	int i, pos;
+	pos = fh->offset[0] - 53;
+	for(i=1; i< pos;i++){
+		numbytes = read(imagen, basura, sizeof(unsigned char));
+	}
+	numbytes = read(imagen, imgdata, sizeof(unsigned char)*ih->imgsize[0]);	
+
+  	close(imagen);
+  	return imgdata;
+}
+
+void iniciador(bmpFileHeader* fh, bmpInfoHeader* ih){
+	fh->size = (unsigned int*)malloc(sizeof(unsigned int));        /* Tamaño del archivo */
+	fh->resv1 = (unsigned short*)malloc(sizeof(unsigned short));       /* Reservado */
+	fh->resv2 = (unsigned short*)malloc(sizeof(unsigned short));      /* Reservado */
+	fh->offset = (unsigned int*)malloc(sizeof(unsigned int));
+
+
+	ih->headersize = (unsigned int*)malloc(sizeof(unsigned int));      /* Tamaño de la cabecera */
+	ih->width = (unsigned int*)malloc(sizeof(unsigned int));               /* Ancho */
+	ih->height = (unsigned int*)malloc(sizeof(unsigned int));          /* Alto */
+	ih->planes= (unsigned short*)malloc(sizeof(unsigned short));                  /* Planos de color (Siempre 1) */
+	ih->bpp = (unsigned short*)malloc(sizeof(unsigned short));             /* bits por pixel */
+	ih->compress = (unsigned int*)malloc(sizeof(unsigned int));        /* compresión */
+	ih->imgsize = (unsigned int*)malloc(sizeof(unsigned int));     /* tamaño de los datos de imagen */
+	ih->bpmx = (unsigned int*)malloc(sizeof(unsigned int));                /* Resolución X en bits por metro */
+	ih->bpmy = (unsigned int*)malloc(sizeof(unsigned int));                /* Resolución Y en bits por metro */
+	ih->colors = (unsigned int*)malloc(sizeof(unsigned int));              /* colors used en la paleta */
+	ih->imxtcolors = (unsigned int*)malloc(sizeof(unsigned int));      /* Colores importantes. 0 si son todos */
+}
 
 
 
