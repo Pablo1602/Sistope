fbe80e7898e1f7249f07a3842393ade5d0d6234b
Unknown <ricardo.alvarezz@usach.cl>
2018-05-08 17:24:16 -0300

    Creacion de archivos

---
+++ LAB 2/convagris.c
@@ -0,0 +1,8 @@
+#include "Headers.h"
+#include "Funciones.h"
+
+int main(int argc, char const *argv[])
+{
+	
+	return 0;
+}
---
+++ LAB 2/escritura.c
@@ -0,0 +1,8 @@
+#include "Headers.h"
+#include "Funciones.h"
+
+int main(int argc, char const *argv[])
+{
+	/* code */
+	return 0;
+}
